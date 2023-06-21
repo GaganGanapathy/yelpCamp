39b69a9234803a3f207ea7d0be937c021931be26
Gagan <ganapathi.gagan@gmail.com>
2023-06-21 13:17:51 +0530

    bug fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes  = require('./routes/reviews')
 
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 mongoose.connect( dbUrl ,{
     useNewUrlParser: true,
     useUnifiedTopology: true
